59b8b92505944043446a8ce96b1e09a63549eae6
xrr2016 <xiaoranran1993@163.com>
2017-10-23 12:21:57 +0800

    :zap: writing

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { h } from './h'
+
+export const app = (props, container = document.body) => {
+  const root = container.children[0]
+}
---
+++ src/h.js
@@ -0,0 +1,9 @@
+let i
+let stack = []
+
+export const h = (type, props) => {
+  let node
+  let children
+
+  
+}
